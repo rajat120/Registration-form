22c41b072eff0c6da0c9acdc9dbee190bcac3ab0
Rajat Agarwal <arajat970@gmail.com>
2023-05-22 13:01:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// const handleSubmit = () => {
+// 	console.log("click");
+// 	// window.location.href = "https://www.google.com";
+// };
+
+// const el = document.getElementById("btn_submit");
+// el.addEventListener("click", handleSubmit);
